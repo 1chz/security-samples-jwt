97f6ee1adb3b44333d47528dbb97deb13466a1e1
hahn <shirohoo4102@gmail.com>
2023-03-22 22:56:45 +0900

    refactor SecurityConfig.kt

--- src/main/kotlin/io/github/shirohoo/samples/security/jwt/SecurityConfig.kt
+++ src/main/kotlin/io/github/shirohoo/samples/security/jwt/SecurityConfig.kt
@@ -1,6 +1,5 @@
 package io.github.shirohoo.samples.security.jwt
 
-import com.nimbusds.jose.jwk.JWK
 import com.nimbusds.jose.jwk.JWKSet
 import com.nimbusds.jose.jwk.RSAKey
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet
@@ -11,9 +10,7 @@ import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.oauth2.jwt.JwtDecoder
-import org.springframework.security.oauth2.jwt.JwtEncoder
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint
@@ -26,38 +23,32 @@ import java.security.interfaces.RSAPublicKey
 @Configuration
 class SecurityConfig {
     @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        return http
-            .authorizeHttpRequests { it.anyRequest().authenticated() }
-            .csrf { it.ignoringRequestMatchers("/token") }
-            .httpBasic(Customizer.withDefaults())
-            .oauth2ResourceServer { it.jwt() }
-            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
-            .exceptionHandling {
-                it.authenticationEntryPoint(BearerTokenAuthenticationEntryPoint())
-                    .accessDeniedHandler(BearerTokenAccessDeniedHandler())
-            }.build()
-    }
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain = http
+        .authorizeHttpRequests { it.anyRequest().authenticated() }
+        .csrf { it.ignoringRequestMatchers("/token") }
+        .httpBasic(Customizer.withDefaults())
+        .oauth2ResourceServer { it.jwt() }
+        .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+        .exceptionHandling {
+            it.authenticationEntryPoint(BearerTokenAuthenticationEntryPoint())
+                .accessDeniedHandler(BearerTokenAccessDeniedHandler())
+        }.build()
 
     @Bean
-    fun jwtDecoder(@Value("\${security.key.public}") rsaPublicKey: RSAPublicKey): JwtDecoder {
-        return NimbusJwtDecoder.withPublicKey(rsaPublicKey).build()
-    }
+    fun imMemoryUsers() = User
+        .withUsername("username")
+        .password("{noop}password")
+        .authorities("demo:read")
+        .build()
+        .let { InMemoryUserDetailsManager(it) }
+
+    @Bean
+    fun jwtDecoder(@Value("\${security.key.public}") rsaPublicKey: RSAPublicKey): JwtDecoder = NimbusJwtDecoder.withPublicKey(rsaPublicKey).build()
 
     @Bean
     fun jwtEncoder(
         @Value("\${security.key.public}") rsaPublicKey: RSAPublicKey,
         @Value("\${security.key.private}") rsaPrivateKey: RSAPrivateKey
-    ): JwtEncoder {
-        val jwk: JWK = RSAKey.Builder(rsaPublicKey).privateKey(rsaPrivateKey).build()
-        return NimbusJwtEncoder(ImmutableJWKSet(JWKSet(jwk)))
-    }
-
-    @Bean
-    fun imMemoryUsers(): UserDetailsService = InMemoryUserDetailsManager(
-        User.withUsername("username")
-            .password("{noop}password")
-            .authorities("demo:read")
-            .build()
-    )
+    ) = RSAKey.Builder(rsaPublicKey).privateKey(rsaPrivateKey).build()
+        .let { NimbusJwtEncoder(ImmutableJWKSet(JWKSet(it))) }
 }
