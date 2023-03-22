db6a120e5eaad92d310b42f2bcd53cbe6395a08a
shirohoo <shirohoo4102@gmail.com>
2023-03-22 22:44:36 +0900

    rename main class

--- src/main/kotlin/io/github/shirohoo/samples/security/jwt/SecurityJwtApplication.kt
+++ src/main/kotlin/io/github/shirohoo/samples/security/jwt/SecurityJwtApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class JWTSecurityApplication
+class SecurityJwtApplication
 
 fun main(args: Array<String>) {
-    runApplication<JWTSecurityApplication>(*args)
+    runApplication<SecurityJwtApplication>(*args)
 }
--- src/test/kotlin/io/github/shirohoo/samples/security/jwt/SecurityJwtApplicationTests.kt
+++ src/test/kotlin/io/github/shirohoo/samples/security/jwt/SecurityJwtApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class JWTSecurityApplicationTests {
+class SecurityJwtApplicationTests {
     @Test
     fun contextLoads() {
     }
